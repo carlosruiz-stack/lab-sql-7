488c2c269388027a5fb10fdd2785768506824d9b
carlosruiz-stack <122636522+carlosruiz-stack@users.noreply.github.com>
2023-02-28 21:03:56 +0100

    Update sakila-lab.7.sql
    1. In the table actor, which are the actors whose last names are not repeated? For example if you would sort the data in the table actor by last_name, you would see that there is Christian Arkoyd, Kirsten Arkoyd, and Debbie Arkoyd. These three actors have the same last name. So we do not want to include this last name in our output. Last name "Astaire" is present only one time with actor "Angelina Astaire", hence we would want this in our output list.
    
    2. Which last names appear more than once? We would use the same logic as in the previous question but this time we want to include the last names of the actors where the last name was present more than once
    
    3. Using the rental table, find out how many rentals were processed by each employee.
    4. Using the film table, find out how many films were released each year.
    5. Using the film table, find out for each rating how many films were there.
    6. What is the mean length of the film for each rating type. Round off the average lengths to two decimal places
    7. Which kind of movies (rating) have a mean duration of more than two hours?

--- sakila-lab.7.sql
+++ sakila-lab.7.sql
@@ -1,7 +1,5 @@
 USE sakila;
 
-USE sakila;
-
 #1
 SELECT last_name 
 FROM actor
@@ -20,7 +18,6 @@ FROM rental
 GROUP BY staff_id;
 
 #4 
-
 SELECT release_year, COUNT(release_year) FROM film
 GROUP BY release_year;
 
@@ -29,12 +26,10 @@ SELECT rating, COUNT(rating) FROM film
 GROUP BY rating;
 
 #6
-
 SELECT rating, AVG(length) FROM film
 GROUP BY rating; 
 
 #7
-
 SELECT rating, AVG(length) FROM film
 GROUP BY rating
 HAVING AVG(length) > 120; 
